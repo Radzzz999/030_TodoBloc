a719db7ad1a272b042c13a2becd7373dfe0e35ed
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2025-05-09 02:42:30 +0700

    buat model Todo dengan atribut title, date, dan isCompleted

---
+++ lib/model/todo.dart
@@ -0,0 +1,14 @@
+todo.dart
+
+class Todo{
+  String title;
+  DateTime date;
+  bool isCompleted;
+  Todo({
+    required this.title,
+    required this.date,
+    required this.isCompleted,
+  });
+
+  
+}
